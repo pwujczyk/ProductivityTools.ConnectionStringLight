ca0b78a8eab66301ffb5a98b4c9c6902ead3bfff
Pawel Wujczyk <pwujczyk@hotmail.com>
2018-02-18 21:26:56 +0100

    initial commit

---
+++ ConnectionStringHelper/ConnectionString.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConnectionStringLightHelper
+{
+    public static class ConnectionStringLight
+    {
+        /// <summary>
+        /// Returns connection string only with sql server name. Used for creating new database.
+        /// Example: Data Source=.\sqlServerInstance;Integrated Security=True
+        /// </summary>
+        /// <param name="dataSource"></param>
+        /// <returns></returns>
+        public static string GetSqlDataSourceConnectionString(string dataSource)
+        {
+            return GetSQLDataSource(dataSource).ToString();
+        }
+
+        /// <summary>
+        /// Returns connection string with datasource and database name
+        /// Example: Data Source=.\sqlServerInstance;Initial Catalog=DatabaseName;Integrated Security=True
+        /// </summary>
+        /// <param name="datasource"></param>
+        /// <param name="databaseName"></param>
+        /// <returns></returns>
+        public static string GetSqlServerConnectionString(string datasource, string databaseName)
+        {
+            var connection = GetDataSource(datasource).AddIntegratedSecurity().AddInitialCatalog(databaseName);
+            return connection.ToString();
+        }
+
+        private static SqlConnectionStringBuilder GetSQLDataSource(string dataSource)
+        {
+            var r = GetDataSource(dataSource).AddIntegratedSecurity();
+            return r;
+        }
+
+        private static SqlConnectionStringBuilder GetDataSource(string dataSource)
+        {
+            SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder();
+            sqlBuilder.DataSource = dataSource;
+            return sqlBuilder;
+        }
+
+        private static SqlConnectionStringBuilder AddIntegratedSecurity(this SqlConnectionStringBuilder connectionStringBuilder)
+        {
+            connectionStringBuilder.IntegratedSecurity = true;
+            return connectionStringBuilder;
+        }
+
+        private static SqlConnectionStringBuilder AddInitialCatalog(this SqlConnectionStringBuilder connectionStringBuilder, string initialCatalog)
+        {
+            connectionStringBuilder.InitialCatalog = initialCatalog;
+            return connectionStringBuilder;
+        }
+    }
+}
---
+++ ConnectionStringHelperTests/ConnectionStringTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ConnectionStringLightHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConnectionStringLightHelperTests
+{
+    [TestClass]
+    public class ConnectionStringTests
+    {
+        [TestMethod]
+        public void SQLDataSourceConnectionString()
+        {
+            var x=ConnectionStringLight.GetSqlDataSourceConnectionString(".");
+            Assert.AreEqual(x, "Data Source=.;Integrated Security=True");
+
+            var y = ConnectionStringLight.GetSqlServerConnectionString(".", "dbName");
+            Assert.AreEqual(y, "Data Source=.;Initial Catalog=dbName;Integrated Security=True");
+        }
+    }
+}
